61bef0d2ce11280b54122a45ad21dc7af8bbc1da
Mr--Prakash6381 <prakash875478@gmail.com>
2024-12-25 22:34:25 +0530

    Array and functions in this file

---
+++ day_2/array_ex_2.php
@@ -0,0 +1,9 @@
+<?php 
+$name=array("Apple","Banana","Orange","Maduali");
+$arraylangh=count($name);
+for($i=0;$i<$arraylangh;$i++)
+{
+    echo $name[$i];
+    echo "__";
+}
+?>
---
+++ day_2/funtion_argument.php
@@ -0,0 +1,7 @@
+<?php 
+function abc($name){
+    echo "Function wtih argument__",$name ,"<br>";
+}
+abc("Apple");
+abc("Banana");
+?>€
