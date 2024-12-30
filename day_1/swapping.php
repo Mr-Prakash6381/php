2b39f41058222f17213270c8eeaf3ac74984ec36
Mr--Prakash6381 <prakash875478@gmail.com>
2024-12-30 06:09:21 +0530

    swapping variable

---
+++ day_1/swapping.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Swapping</title>
+</head>
+<body>
+    <h1>Swapping</h1>
+    <?php
+    
+    $a=100;
+    $b=200; 
+    echo "Before Swapping  : <br>";
+    echo "A : $a <br>";
+    echo "B : $b <br>";
+    $c=$a;
+    $a=$b;
+    $b=$c;
+    echo "After Swapping : <br>";
+    echo "A : $a <br>";
+    echo "B : $b <br>";
+    
+    ?>
+    
+</body>
+</html>
