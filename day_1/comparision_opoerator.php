4d010db870132960bd8d896d37a4395b256b82d5
Mr--Prakash6381 <prakash875478@gmail.com>
2024-12-30 06:08:40 +0530

    comparision_opoerator

---
+++ day_1/comparision_opoerator.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Comparision Operator</title>
+</head>
+<body>
+    <h1>Comparision Operator</h1>
+    <?php
+    $a=12;
+    $b=110;
+    if($a>$b){
+        echo "A is a greathan number";
+    }
+    else{
+        echo "B is a grethan number <br>";
+    }
+    $tamil=35;
+    $English=45;
+    if($tamil>=35 and $English>=35 )
+    {
+        echo "<br>Tamil and English both result pass <br>";
+    }
+    else{
+        echo "<br>Tamil and English both one result faill";
+    }
+    ?>
+    
+</body>
+</html>
