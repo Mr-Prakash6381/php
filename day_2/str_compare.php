e7247c522b9a3266c4c6db6483d585ea7bcb97ed
Mr--Prakash6381 <prakash875478@gmail.com>
2025-01-03 20:08:35 +0530

    String Compare

---
+++ day_2/str_compare.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>String Function</title>
+</head>
+
+<body>
+    <h1>String compare the two string</h1>
+    <?php
+    $str="Hello";
+    $str1="Hello";
+    $str2="ABC";
+    echo strcmp($str,$str1);
+    echo "<br>";
+    echo strcmp($str,$str2);
+    ?>
+</body>
+
+</html>
