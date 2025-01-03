235eb8f29bee22f481b2ab8fad812f87f1b3b4f4
Mr--Prakash6381 <prakash875478@gmail.com>
2025-01-03 20:10:39 +0530

    String Replace

---
+++ day_2/str_replace.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>String Function</title>
+</head>
+
+<body>
+    <h1>String Replace the string</h1>
+    <?php
+    $str="Hello";
+    echo str_replace('Hello','Welcome to PHP',$str);
+    
+    ?>
+</body>
+
+</html>
