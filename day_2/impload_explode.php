1988f403b901d83021f0b34faa6ea4f334441a79
Mr--Prakash6381 <prakash875478@gmail.com>
2025-01-03 20:07:23 +0530

    Impload Expload

---
+++ day_2/impload_explode.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Impload Explode</title>
+</head>
+
+<body>
+    <h1>Impload Explode</h1>
+    <?php
+    $a="Ram,Sam,Ravi,Sunder,Joes,Stanley,Abitha";
+    $name=explode(',',$a);
+    echo "<pre>";
+    print_r($name);
+    
+    ?>
+
+</body>
+
+</html>
