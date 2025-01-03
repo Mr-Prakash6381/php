133e97c58650180b8adac63d53eb4243671e1c01
Mr--Prakash6381 <prakash875478@gmail.com>
2025-01-03 20:11:45 +0530

    String Find the Length

---
+++ day_2/string_length.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>String Function</title>
+</head>
+
+<body>
+    <h1>String Function How to find the string length</h1>
+    <?php
+    $str="Welcome to php";
+    echo "Find the String Length : ";
+    echo strlen($str);
+    
+    ?>
+</body>
+
+</html>
