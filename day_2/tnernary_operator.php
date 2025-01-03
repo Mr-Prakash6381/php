99a50300cb09fdef43123b3656515dfe2ead87f0
Mr--Prakash6381 <prakash875478@gmail.com>
2025-01-03 20:12:21 +0530

    Tnernary Operator

---
+++ day_2/tnernary_operator.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ternary Operator</title>
+</head>
+
+<body>
+    <h1>Ternary Operator</h1>
+    <?php
+    $a=78;
+    $b=86;
+    $c=$a>$b ? $a:$b;
+    echo "The Greatest of Two Number is : $c"
+    
+    ?>
+</body>
+
+</html>
