c5712efff11f4a548da47a0ffea08ccefb17ad53
Mr--Prakash6381 <prakash875478@gmail.com>
2025-01-03 20:09:04 +0530

    String Count

---
+++ day_2/str_count.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>String Function</title>
+</head>
+
+<body>
+    <h1>Count the string</h1>
+    <?php
+    $str="Count the string function ";
+    echo str_word_count($str);
+    ?>
+
+</body>
+
+</html>
