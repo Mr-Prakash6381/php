17c6feaf36474d95e9c9e4d880c570de1b2bd7a3
Mr--Prakash6381 <prakash875478@gmail.com>
2025-01-03 20:09:53 +0530

    String Lowercase or  Upercase

---
+++ day_2/str_lowercase_upercase.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>String Function</title>
+</head>
+
+<body>
+    <h1>String Lowercase and Upercase</h1>
+    <?php
+    $str="Upper";
+    $str1="LOWER";
+    echo "Lowercase : ";
+    echo strtolower($str1);
+    echo "<br> Uppercase : ";
+    echo strtoupper($str);
+    ?>
+
+</body>
+
+</html>
